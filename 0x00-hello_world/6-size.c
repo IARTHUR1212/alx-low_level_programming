c324afb5692290b73a066812d5d4501eff3ae517
IARTHUR1212 <greatike2010@yahoo.com>
2023-07-15 14:19:14 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Retun: 0 on success
+ * */
+
+int main(void)
+{
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n, sizeof(float));
+	return (0);
+}
